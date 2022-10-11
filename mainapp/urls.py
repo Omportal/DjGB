8dac1d63584589b78e8ff6b8ea1bd1b3858c5201
Slava <omportalmusic@gmail.com>
2022-10-12 00:00:28 +0300

    'lesson_3'

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', MainView.as_view(), name='index'),
     path('login', LoginView.as_view(), name='login'),
     path('news', NewsView.as_view(), name='news'),
+    path("news/<int:page>/", NewsWithPaginatorView.as_view(),name="news_paginator"),
     path('doc_site', DocSiteView.as_view(), name='doc_site'),
     path('courses_list', CoursesListView.as_view(), name='courses_list'),
     path('contacts', ContactsView.as_view(), name='contacts'),
